08561a8a6064cc2d0ace650ca7e1c91bb9556808
Shardul Vaidya <cam.v737@gmail.com>
2021-05-17 22:39:50 -0400

    WIP: ZED Record started

--- zed_record/src/zed_record.cpp
+++ zed_record/src/zed_record.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "zed_record.h"
+#include <sl/Camera.hpp>
+
+#include "utils.h"
+
+
+using namespace sl;
+using namespace std;
+
+void print(string msg_prefix, ERROR_CODE err_code = ERROR_CODE::SUCCESS, string msg_suffix = "");
+void parseArgs(int argc, char **argv, sl::InitParameters& param);
--- zed_record/src/zed_record.h
+++ zed_record/src/zed_record.h
@@ -1,6 +1,26 @@
 //
 // Created by sherlock on 12/05/2021.
 //
+#pragma once
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "csignal"
+
+static bool exit_app = false;
+
+void nix_exit_handler(int s) {
+    exit_app = true;
+}
+
+void SetCtrlHandler () {
+#ifdef __linux__
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = nix_exit_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+#endif
+}
 
 #ifndef C_UNDERWATERVLC_ZED_RECORD_H
 #define C_UNDERWATERVLC_ZED_RECORD_H
