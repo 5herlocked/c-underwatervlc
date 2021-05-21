2c49b9ac01d76076e81ff79ef1c9b23c419d9f25
Shardul Vaidya <cam.v737@gmail.com>
2021-05-20 22:16:39 -0400

    WIP: SVO_Export things

--- svo_export/src/svo_export.cpp
+++ svo_export/src/svo_export.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "svo_export.h"
+#include "iostream"
+#include "sstream"
+#include "opencv2/opencv.hpp"
+#include "utils.h"
+#include "sl/Camera.hpp"
+
+using namespace sl;
+using namespace std;
+
+enum APP_TYPE {
+    FOLDER_OF_VIDS,
+    COMPLETE_DECOMPOSITION
+};
+
+void print(string msg_prefix, ERROR_CODE err_code = ERROR_CODE::SUCCESS, string msg_suffix = "");
+
+int main(int argc, char* argv[]) {
+
+}
