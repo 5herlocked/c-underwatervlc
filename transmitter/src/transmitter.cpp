b43d340acd34bfb685c6e402e5656fb84cf817a5
Shardul Vaidya <cam.v737@gmail.com>
2021-06-23 00:44:47 -0400

    Created a lot of the code to use libgpiod for the transmitters

--- transmitter/src/transmitter.cpp
+++ transmitter/src/transmitter.cpp
@@ -4,54 +4,162 @@
 // Modified and maintained by Sherlocked (github.com/5herlocked)
 // For MORSELab as part of his responsibilities
 
-#include "transmitter.h"
-#include <cstdio>
+/*
+ * Uses the GPIO_BOARD 12 pin as included in the program
+ * Has the ability to generate, transmit and log as configured by the user
+ * It only relies on system libraries available on an nVidia Jetson Nano Developer Kit
+ */
+
 #include <cstdlib>
 #include <chrono>
 #include <thread>
 #include <optional>
-#include <getopt.h>
-#include <unistd.h>
-#include <sys/fcntl.h>
-#include <sys/mman.h>
 #include <iostream>
+
+#include "transmitter.h"
+#include "getopt.h"
+#include "unistd.h"
+#include "gpiod.h"
 
 using namespace std;
 
+// Define enums for standardisation
 enum APP_TYPE {
     STATE,
     RANDOM,
     // TODO: Just add elements in here as the app gets more complicated
 };
 
-enum GPIO_STATE {
+enum GPIO {
     OFF,
     ON,
 };
 
+
+// Main data holding struct to manage the entire configuration of the application
+// Realistically there will only be one that'll be created
+// but easier to just declare as a full struct
 struct Configuration {
-    optional<APP_TYPE> type;
-    optional<GPIO_STATE> state;
-    optional<int> bits;
-    optional<int> frequency;
-    optional<int> cycles;
+    optional<APP_TYPE> type{};
+    optional<GPIO> state{};
+    optional<int> bits{};
+    optional<int> frequency{};
+    optional<int> cycles{};
 };
 
-void parseArgs(int argc, char **argv, Configuration& config);
-void transmit();
+// function definitions
+void parseArgs(int argc, char **argv, Configuration &config);
+
+// [[maybe_unused]] void transmitRaw(const Configuration &config);
+
+void transmit(const Configuration &config);
+
+void setState(const Configuration &config);
+
 void showUsage();
 
-int main(int argc, char* argv[])
-{
-    transmit();
-//    Configuration appConfig{};
-//    parseArgs(argc, argv, appConfig);
+optional<GPIO> toGPIO(const string &input);
+
+int main(int argc, char *argv[]) {
+    Configuration appConfig{};
+    parseArgs(argc, argv, appConfig);
+
+    if (appConfig.type.has_value() && appConfig.type.value() == APP_TYPE::STATE) {
+        setState(appConfig);
+    } else if (appConfig.type.has_value() && appConfig.type.value() == APP_TYPE::RANDOM) {
+        transmit(appConfig);
+    } else {
+        // Incorrectly configured
+    }
 
     return 0;
 }
 
-void transmit() {
-    long frequency = 1/4;
+/*
+ * Parses commandline arguments using the GNU C library function getopt_long
+ * Reference: https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Option-Example.html
+ */
+void parseArgs(int argc, char **argv, Configuration &config) {
+    // TODO: make this not use getopt
+    int opt;
+    struct option long_options[] = {
+            {"help",      no_argument,       nullptr, 'h'},
+            {"state",     required_argument, nullptr, 's'},
+            {"random",    optional_argument, nullptr, 'r'},
+            {"frequency", required_argument, nullptr, 'f'},
+            {"cycles",    required_argument, nullptr, 'c'},
+            {nullptr,     no_argument,       nullptr, 0}
+    };
+    int optionIdx = 0;
+    while ((opt = getopt_long(argc, argv, "hs:r:f:c:", long_options, &optionIdx)) != -1) {
+        switch (opt) {
+            case 0:
+                //TODO: No arguments, default config
+                break;
+            case 'h':
+                showUsage();
+                exit(0);
+            case 's':
+                if (config.type.has_value() && config.type.value() == APP_TYPE::RANDOM) {
+                    cout << "The state flag takes precedence. Using the given state: " << optarg << endl;
+                }
+
+                config.type = APP_TYPE::STATE;
+                config.state = toGPIO(optarg);
+                break;
+            case 'r':
+                if (config.type.has_value() && config.type.value() == APP_TYPE::STATE) {
+                    cout << "The state flag takes precedence. Ignoring the random bits flag" << endl;
+                }
+                config.type = APP_TYPE::RANDOM;
+                config.bits = strtol(optarg, nullptr, 10);
+                break;
+            case 'f':
+                config.frequency = strtol(optarg, nullptr, 10);
+                break;
+            case 'c':
+                config.cycles = strtol(optarg, nullptr, 10);
+                break;
+            default:
+                cout << "Unknown option: <" << opt << endl;
+                break;
+        }
+    }
+}
+
+// Goes from string to enum GPIO
+// returns nullopt if the parameter doesn't correspond to on OR off
+optional<GPIO> toGPIO(const string &input) {
+    if (input == "on" || input == "ON") {
+        return GPIO::ON;
+    } else if (input == "off" || input == "OFF") {
+        return GPIO::OFF;
+    }
+    return nullopt;
+}
+
+void showUsage() {
+
+}
+
+void setState(const Configuration &config) {
+
+}
+
+/*
+ * Transmits using the sysfs method of gpio manipulation
+ * Should be more than fast enough
+ * Reference
+ */
+
+void transmit(const Configuration &config) {
+
+}
+
+// optional TODO: figure out memory manipulation
+/*
+[[maybe_unused]] void transmitRaw() {
+    long frequency = 1 / 4;
     //  read physical memory (needs root)
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (fd < 0) {
@@ -62,7 +170,7 @@ void transmit() {
 
     //  map a particular physical address into our address space
     int pagesize = getpagesize();
-    int pagemask = pagesize-1;
+    int pagemask = pagesize - 1;
     //  This page will actually contain all the GPIO controllers, because they are co-located
     void *base = mmap(nullptr, pagesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (GPIO_3 & ~pagemask));
     if (base == nullptr) {
@@ -71,7 +179,7 @@ void transmit() {
     }
 
     //  set up a pointer for convenient access -- this pointer is to the selected GPIO controller
-    auto volatile *pin = (GPIO_mem volatile *)((char *)base + (GPIO_3 & pagemask));
+    auto volatile *pin = (GPIO_mem volatile *) ((char *) base + (GPIO_3 & pagemask));
 
     int port = 0;
 
@@ -111,42 +219,4 @@ void transmit() {
     munmap(base, pagesize);
     close(fd);
 }
-
-//void parseArgs(int argc, char **argv, Configuration& config) {
-//    // TODO: make this not use getopt
-//    int opt;
-//    struct option long_options[] = {
-//            {"help", no_argument, nullptr, 'h'},
-//            {"state", required_argument, nullptr, 's'},
-//            {"random", optional_argument, nullptr, 'r'},
-//            {"frequency", optional_argument, nullptr, 'f'},
-//            {"cycles", optional_argument, nullptr, 'c'},
-//            {nullptr, 0, nullptr, 0}
-//    };
-//    int optionIdx = 0;
-//    while ((opt = getopt_long(argc, argv, "hs:r:f:c:", long_options, &optionIdx)) != -1) {
-//        switch (opt) {
-//            case 0:
-//                // TODO: No arguments, default options
-//                break;
-//            case 'h':
-//                showUsage();
-//                exit(0);
-//            case 's':
-//                // TODO: Configure the app to set the state of the Transmitter
-//                break;
-//            case 'r':
-//                // TODO: Configure the app to set the random bits
-//                break;
-//            case 'f':
-//                // TODO: Configure the app to set the frequency
-//                break;
-//            case 'c':
-//                // TODO: Configure the app to set the cycles
-//                break;
-//            default:
-//                cout << "Unknown option: <" << opt << endl;
-//                break;
-//        }
-//    }
-//}
+*/
