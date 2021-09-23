0b026c014b4ee3f3c87ffca73987c095724033c8
Shardul Vaidya <cam.v737@gmail.com>
2021-09-22 21:28:38 -0400

    moved transmitter files

--- julia/pixel-intensity-variance.jl
+++ julia/pixel-intensity-variance.jl
@@ -3,20 +3,23 @@ gr()
 
 # load all the available data
 begin
-    data_location = String(pwd()) * "\\julia\\test-set\\"
+    data_location = String(pwd()) * "\\test-set\\"
     available_files = readdir(data_location)
 end
 
 begin
     temp_files = available_files |> @filter(occursin("c", split(_, '.')[1])) |> collect
-    
-    temp_dF = DataFrame(CSV.File(data_location * temp_files[1]))
 
-    temp_list_off = temp_dF |> @filter(_.bit == 0) |> DataFrame
-    temp_list_on = temp_dF |> @filter(_.bit == 1) |> DataFrame
+    for file in temp_files
+        title = split(file, '.')[1]
+        temp_dF = DataFrame(CSV.File(data_location * file))
 
-    plotandExport(temp_list_off, "Pixel Intensities OFF, 28c", "Pixel Intensity OFF 28c")
-    plotandExport(temp_list_on, "Pixel Intensities ON, 28c", "Pixel Intensity ON 28c")
+        temp_list_off = temp_dF |> @filter(_.bit == 0) |> DataFrame
+        temp_list_on = temp_dF |> @filter(_.bit == 1) |> DataFrame
+
+        plotandExport(temp_list_off, title*"OFF", title*"OFF")
+        plotandExport(temp_list_on, title*"ON", title*"ON")
+    end
 end
 
 begin
@@ -26,12 +29,46 @@ begin
         return
     end
 
-    salinity_dF = DataFrame(CSV.File(data_location*salinity_files[1]))
+    for file in salinity_files
+        title = split(file, '.')[1]
+        temp_dF = DataFrame(CSV.File(data_location*file))
 
-    salinity_list_off = salinity_dF |> @filter(_.bit == 0) |> DataFrame
-    salinity_list_on = salinity_dF |> @filter(_.bit == 1) |> DataFrame 
+        temp_list_off = temp_dF |> @filter(_.bit == 0) |> DataFrame
+        temp_list_on = temp_dF |> @filter(_.bit == 1) |> DataFrame
 
-    plotandExport(salinity_list_off, "Pixel Intensity OFF")
+        plotandExport(temp_list_off, title*"OFF", title*"OFF")
+        plotandExport(temp_list_on, title*"ON", title*"ON")
+    end
+end
+
+begin
+    ph_files = available_files |> @filter(occursin("ph", split(_, '.')[1])) |> collect
+
+    for file in ph_files
+        title = split(file, '.')[1]
+        temp_dF = DataFrame(CSV.File(data_location*file))
+
+        temp_list_off = temp_dF |> @filter(_.bit == 0) |> DataFrame
+        temp_list_on = temp_dF |> @filter(_.bit == 1) |> DataFrame
+
+        plotandExport(temp_list_off, title*"OFF", title*"OFF")
+        plotandExport(temp_list_on, title*"ON", title*"ON")
+    end
+end
+
+begin
+    turbid_files = available_files |> @filter(occursin("g", split(_, '.')[1])) |> collect
+
+    for file in turbid_files
+        title = split(file, '.')[1]
+        temp_dF = DataFrame(CSV.File(data_location*file))
+
+        temp_list_off = temp_dF |> @filter(_.bit == 0) |> DataFrame
+        temp_list_on = temp_dF |> @filter(_.bit == 1) |> DataFrame
+
+        plotandExport(temp_list_off, title*"OFF", title*"OFF")
+        plotandExport(temp_list_on, title*"ON", title*"ON")
+    end
 end
 
 function plotandExport(df, title, fn)
@@ -39,8 +76,9 @@ function plotandExport(df, title, fn)
         title=title,
         label=["Blue" "Red" "Green"],
         ["Blue" "Red" "Green"],
-        [df.blue df.red df.green]
+        [df.blue df.red df.green],
+        ylims = (0, 255)
     )
 
-    savefig(temp, pwd()*"\\julia\\"*fn*".svg")
+    savefig(temp, pwd()*"\\export\\"*fn*".svg")
 end
